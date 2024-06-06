9f986cd1d72724c02d3eca37e6f59e241ad4e411
gusgonza <19624236-gusgonza@users.noreply.gitlab.com>
2024-06-06 12:00:29 +0200

    chore: update application.properties with new datasource driver class name

---
+++ test-2-V2-sol-ABP-WebReservasVue-recu_prog/src/main/java/abp/project/v2Videojuegos/V2VideojuegosApplication.java
@@ -0,0 +1,13 @@
+package abp.project.v2Videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2VideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2VideojuegosApplication.class, args);
+	}
+
+}
